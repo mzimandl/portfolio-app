d9260e7c09ea5230a5f64cb8188c75462b101e23
Martin Zimandl <martin.zimandl@gmail.com>
2022-03-01 11:46:54 +0100

    price chart improvement - dividends and splits

--- src/sections/Prices.tsx
+++ src/sections/Prices.tsx
@@ -1,5 +1,5 @@
 import { Box, FormControl, InputLabel, Select, MenuItem, Slider } from '@mui/material';
-import { CartesianGrid, XAxis, YAxis, Tooltip, Area, ResponsiveContainer, ComposedChart, Bar, Cell, Line } from 'recharts';
+import { CartesianGrid, XAxis, YAxis, Tooltip, Area, ResponsiveContainer, ComposedChart, Bar, Cell, Line, Scatter } from 'recharts';
 import { AbstractSection, SectionProps } from '../common';
 import { InstrumentDataRow } from './Settings';
 
@@ -102,9 +102,11 @@ export class Prices extends AbstractSection<PricesProps, PricesState> {
                                     {this.state.chartData.map((item, index) =>
                                         <Cell key={`cell-${index}`} fill={item['close'] >= item['open'] ? '#56DD00' : '#FF3737'} />
                                     )}
-                                </Bar>
+                                </Bar>,
                             ]
                         }
+                        <Scatter name="Dividends" dataKey={obj => obj['dividends'] > 0 ? obj['dividends'] : null} shape="diamond" fill="#8884d8" />
+                        <Scatter name="Splits" dataKey={obj => obj['splits'] > 0 ? obj['splits'] : null} shape="wye" fill="#8884d8" />
                         <XAxis dataKey="date" angle={30} dy={30} dx={6} height={80}/>
                         <YAxis tickFormatter={(v, i) => this.formatCurrency(v, this.state.currency)||''} width={100}/>
                         <Tooltip formatter={(value: number) => this.formatCurrency(value, this.state.currency)} />
