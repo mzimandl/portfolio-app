5b8a87422ac65ff4b44ad0a82393f1c60d4f47f0
Martin Zimandl <martin.zimandl@gmail.com>
2022-03-08 11:38:29 +0100

    separate settings add components

--- src/sections/Settings.tsx
+++ src/sections/Settings.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { AbstractSection, SectionProps } from '../common';
 import { Table, TableBody, TableHead, TableContainer, TableRow, TableCell, Box, IconButton, FormControl, Grid, InputLabel, Select, MenuItem, Autocomplete, CardContent, Card } from '@mui/material';
 import TextField from '@mui/material/TextField';
@@ -26,11 +27,6 @@ interface SettingsState {
     instruments: Array<InstrumentDataRow>;
     currencies: Array<string>;
     types: Array<string>;
-    new: {
-        instrument: NewInstrument,
-        currency: string|null,
-        type: string|null,
-    }
 }
 
 type CurrenciesResponse = Array<string>;
@@ -38,6 +34,94 @@ type TypesResponse = Array<string>;
 export type InstrumentsResponse = Array<InstrumentDataRow>;
 
 interface SettingsProps {}
+interface NewStringProps {
+    stringName: string;
+    addHandler: (value:string|null) => void;
+}
+interface NewInstrumentProps {
+    types: Array<string>;
+    currencies: Array<string>;
+    addInstrument: (newInstrument:NewInstrument) => void;
+}
+
+class NewStringTableRow extends React.Component<NewStringProps, {value: string|null}> {
+
+    constructor(props: NewStringProps) {
+        super(props);
+        this.state = {value: null};
+    }
+
+    render() {
+        return <TableRow>
+            <TableCell>
+                <TextField label={this.props.stringName} variant="outlined" size='small' margin='none'
+                           fullWidth value={this.state.value} onChange={(e) => this.setState({value: e.target.value})} />
+            </TableCell>
+            <TableCell>
+                <IconButton onClick={e => this.props.addHandler(this.state.value)}><AddBox/></IconButton>
+            </TableCell>
+        </TableRow>
+    }
+}
+
+class NewInstrumentTableRow extends React.Component<NewInstrumentProps, NewInstrument> {
+
+    constructor(props: NewInstrumentProps) {
+        super(props);
+        this.state = {
+            ticker: null,
+            currency: null,
+            type: null,
+            evaluation: 'yfinance',
+            eval_param: null,
+        };
+    }
+
+    render() {
+        return <TableRow>
+            <TableCell>
+                <TextField label="Ticker" variant="outlined" size='small' margin='none' fullWidth value={this.state.ticker}
+                onChange={(e) => this.setState({ticker: e.target.value})} />
+            </TableCell>
+            <TableCell>
+                <Autocomplete
+                    value={this.state.currency}
+                    onChange={(e, newValue) => this.setState({currency: newValue})}
+                    options={this.props.currencies}
+                    renderInput={(params) => <TextField {...params} size='small' label="Currency" />}
+                />
+            </TableCell>
+            <TableCell>
+                <Autocomplete
+                    value={this.state.type}
+                    onChange={(e, newValue) => this.setState({type: newValue})}
+                    options={this.props.types}
+                    renderInput={(params) => <TextField {...params} size='small' label="Type" />}
+                />
+            </TableCell>
+            <TableCell>
+                <FormControl fullWidth size='small'>
+                    <InputLabel id="vmode-select-label">Evaluation</InputLabel>
+                    <Select
+                        labelId='vmode-select-label'
+                        value={this.state.evaluation}
+                        label="Value mode"
+                        onChange={(e) => this.setState({evaluation: e.target.value as EvaluationType})}
+                    >
+                        <MenuItem value="yfinance">yfinance</MenuItem>
+                        <MenuItem value="manual">manual</MenuItem>
+                        <MenuItem value="http">http</MenuItem>
+                    </Select>
+                </FormControl>
+            </TableCell>
+            <TableCell>
+                <TextField label="EvalParam" variant="outlined" size='small' margin='none' fullWidth value={this.state.eval_param}
+                onChange={(e) => this.setState({eval_param: e.target.value})} />
+            </TableCell>
+            <TableCell><IconButton onClick={e => this.props.addInstrument(this.state)}><AddBox/></IconButton></TableCell>
+        </TableRow>
+    }
+}
 
 export class Settings extends AbstractSection<SettingsProps, SettingsState> {
 
@@ -49,17 +133,6 @@ export class Settings extends AbstractSection<SettingsProps, SettingsState> {
             instruments: [],
             currencies: [],
             types: [],
-            new: {
-                instrument: {
-                    ticker: null,
-                    currency: null,
-                    type: null,
-                    evaluation: 'yfinance',
-                    eval_param: null,
-                },
-                currency: null,
-                type: null,
-            },
         };
 
         this.addInstrument = this.addInstrument.bind(this);
@@ -97,60 +170,36 @@ export class Settings extends AbstractSection<SettingsProps, SettingsState> {
             .then(types => this.setState({types}));
     }
 
-    addInstrument = () => {
-        if (this.state.new.instrument.ticker && this.state.new.instrument.currency && this.state.new.instrument.type) {
+    addInstrument = (newInstrument: NewInstrument) => {
+        if (newInstrument.ticker && newInstrument.currency && newInstrument.type) {
             this.props.displayProgressBar(true);
-            fetch('/instruments/new', {method: 'POST', body: JSON.stringify(this.state.new.instrument)})
+            fetch('/instruments/new', {method: 'POST', body: JSON.stringify(newInstrument)})
                 .then(res => {
                     this.loadInstruments().then(() => {
-                        this.setState({
-                            new: {
-                                ...this.state.new,
-                                instrument: {
-                                    ticker: '',
-                                    currency: '',
-                                    type: '',
-                                    evaluation: 'yfinance',
-                                    eval_param: '',
-                                }
-                            }
-                        });
                         this.props.displayProgressBar(false)
                     });
                 });
         }
     }
 
-    addCurrency = () => {
-        if (this.state.new.currency) {
+    addCurrency = (currency:string|null) => {
+        if (currency) {
             this.props.displayProgressBar(true);
-            fetch('/currencies/new', {method: 'POST', body: JSON.stringify({currency: this.state.new.currency})})
+            fetch('/currencies/new', {method: 'POST', body: JSON.stringify({currency})})
                 .then(res => {
                     this.loadCurrencies().then(() => {
-                        this.setState({
-                            new: {
-                                ...this.state.new,
-                                currency: '',
-                            }
-                        });
                         this.props.displayProgressBar(false)
                     });
                 });
         }
     }
 
-    addType = () => {
-        if (this.state.new.type) {
+    addType = (type:string|null) => {
+        if (type) {
             this.props.displayProgressBar(true);
-            fetch('/types/new', {method: 'POST', body: JSON.stringify({type: this.state.new.type})})
+            fetch('/types/new', {method: 'POST', body: JSON.stringify({type})})
                 .then(res => {
                     this.loadTypes().then(() => {
-                        this.setState({
-                            new: {
-                                ...this.state.new,
-                                type: '',
-                            }
-                        });
                         this.props.displayProgressBar(false)
                     });
                 });
@@ -166,11 +215,7 @@ export class Settings extends AbstractSection<SettingsProps, SettingsState> {
                             <TableContainer>
                                 <Table size="small">
                                     <TableHead>
-                                        <TableRow>
-                                            <TableCell><TextField label="Currency" variant="outlined" size='small' margin='none' fullWidth value={this.state.new.currency}
-                                                onChange={(e) => this.setState({new: {...this.state.new, currency: e.target.value}})} /></TableCell>
-                                            <TableCell><IconButton onClick={this.addCurrency}><AddBox/></IconButton></TableCell>
-                                        </TableRow>
+                                        <NewStringTableRow stringName="Currency" addHandler={this.addCurrency} />
                                     </TableHead>
                                     <TableBody>
                                         {this.state.currencies.map(
@@ -190,11 +235,7 @@ export class Settings extends AbstractSection<SettingsProps, SettingsState> {
                             <TableContainer>
                                 <Table size="small">
                                     <TableHead>
-                                        <TableRow>
-                                            <TableCell><TextField label="Type" variant="outlined" size='small' margin='none' fullWidth value={this.state.new.type}
-                                                onChange={(e) => this.setState({new: {...this.state.new, type: e.target.value}})} /></TableCell>
-                                            <TableCell><IconButton onClick={this.addType}><AddBox/></IconButton></TableCell>
-                                        </TableRow>
+                                        <NewStringTableRow stringName="Type" addHandler={this.addType} />
                                     </TableHead>
                                     <TableBody>
                                         {this.state.types.map(
@@ -214,48 +255,7 @@ export class Settings extends AbstractSection<SettingsProps, SettingsState> {
                             <TableContainer>
                                 <Table size="small">
                                     <TableHead>
-                                        <TableRow>
-                                            <TableCell>
-                                                <TextField label="Ticker" variant="outlined" size='small' margin='none' fullWidth value={this.state.new.instrument.ticker}
-                                                onChange={(e) => this.setState({new: {...this.state.new, instrument: {...this.state.new.instrument, ticker: e.target.value}}})} />
-                                            </TableCell>
-                                            <TableCell>
-                                                <Autocomplete
-                                                    value={this.state.new.instrument.currency}
-                                                    onChange={(e, newValue) => this.setState({new: {...this.state.new, instrument: {...this.state.new.instrument, currency: newValue}}})}
-                                                    options={this.state.currencies}
-                                                    renderInput={(params) => <TextField {...params} size='small' label="Currency" />}
-                                                />
-                                            </TableCell>
-                                            <TableCell>
-                                                <Autocomplete
-                                                    value={this.state.new.instrument.type}
-                                                    onChange={(e, newValue) => this.setState({new: {...this.state.new, instrument: {...this.state.new.instrument, type: newValue}}})}
-                                                    options={this.state.types}
-                                                    renderInput={(params) => <TextField {...params} size='small' label="Type" />}
-                                                />
-                                            </TableCell>
-                                            <TableCell>
-                                                <FormControl fullWidth size='small'>
-                                                    <InputLabel id="vmode-select-label">Evaluation</InputLabel>
-                                                    <Select
-                                                        labelId='vmode-select-label'
-                                                        value={this.state.new.instrument.evaluation}
-                                                        label="Value mode"
-                                                        onChange={(e) => this.setState({new: {...this.state.new, instrument: {...this.state.new.instrument, evaluation: e.target.value as EvaluationType}}})}
-                                                    >
-                                                        <MenuItem value="yfinance">yfinance</MenuItem>
-                                                        <MenuItem value="manual">manual</MenuItem>
-                                                        <MenuItem value="http">http</MenuItem>
-                                                    </Select>
-                                                </FormControl>
-                                            </TableCell>
-                                            <TableCell>
-                                                <TextField label="EvalParam" variant="outlined" size='small' margin='none' fullWidth value={this.state.new.instrument.eval_param}
-                                                onChange={(e) => this.setState({new: {...this.state.new, instrument: {...this.state.new.instrument, eval_param: e.target.value}}})} />
-                                            </TableCell>
-                                            <TableCell><IconButton onClick={this.addInstrument}><AddBox/></IconButton></TableCell>
-                                        </TableRow>
+                                        <NewInstrumentTableRow types={this.state.types} currencies={this.state.currencies} addInstrument={this.addInstrument} />
                                     </TableHead>
                                     <TableBody>
                                         {this.state.instruments.map(
